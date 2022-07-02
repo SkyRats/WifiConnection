179739188a58235932c34a998f90edc1e7e3fb63
pedro-fuoco <pedrofuoco6@gmail.com>
2022-07-02 19:46:49 -0300

    easier labrador setup

--- labrador-setup.bash
+++ labrador-setup.bash
@@ -5,16 +5,12 @@ cd /home/caninos/WifiConnection
 #Generating connection binary file
 source firstofall.sh
 
-#moving the connect.sh target script
-sudo mv connect.sh /usr/local/bin/connect.sh
-sudo chmod 744 /usr/local/bin/connect.sh
+#Connection for the first time
+echo "---Para essa etapa, certifique-se que o hotspot esta ligado no seu computador pessoal!---"
+./connection
 
-#moving the skyratshotspot-connect.service systemd service
-sudo mv skyratshotspot-connect.service /etc/systemd/system/skyratshotspot-connect.service
-sudo chmod 664 /etc/systemd/system/skyratshotspot-connect.service
-
-#reloading the daemon and enabling the service
-sudo systemctl daemon-reload
-sudo systemctl enable skyratshotspot-connect.service
+#Setting connection priority
+nmcli connection modify SkyratsHotspot connection.autoconnect-priority 10
 
 echo "Se nenhum erro apareceu, basta rebootar a labrador e ela deve conectar com o hotspot automaticamente"
+echo "Ass: Fuco"
