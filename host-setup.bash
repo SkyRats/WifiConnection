fb5c66544bc7b6b15858af7e700cb0b55a34b811
pedro-fuoco <pedrofuoco6@gmail.com>
2022-08-22 20:46:17 -0300

    add host-setup.bash

---
+++ host-setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Generating connection binary file
+source build.sh
+
+#Hosting for the first time
+./newhotspot
+
+echo "Se nenhum erro apareceu, basta rebootar a sbc e ela deve conectar com o hotspot automaticamente"
+echo "Ass: Fuco"
