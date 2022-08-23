b15bfe4c934863b1c7285e9d2eb5d23e8d5d04ef
pedro-fuoco <pedrofuoco6@gmail.com>
2022-08-23 08:13:28 -0300

    updated echo

--- host-setup.bash
+++ host-setup.bash
@@ -9,5 +9,5 @@ source build.sh
 #Hosting for the first time
 ./newhotspot
 
-echo "Se nenhum erro apareceu, basta rebootar a sbc e ela deve conectar com o hotspot automaticamente"
+echo "Se nenhum erro apareceu, um hotspot deve ter sido criado, note que ele nao sera criado por padrao quando o computador ligar, leia o README para mais informacoes"
 echo "Ass: Fuco"
